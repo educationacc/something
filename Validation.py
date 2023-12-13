e985f2d0cfcdc8924b2a320b69ca5cc9b704d446
Francis Law <tinlaw78988@example.com>
2023-12-13 20:27:22 +0100

    second commit : add additional method user_input to Validation.py

--- Validation.py
+++ Validation.py
@@ -1,3 +1,8 @@
 class Validator:
     def __init__(self):
         self.valid_integers = []
+
+    def user_input(self, input_list):
+        for user_input in input_list:
+            if self.is_valid_positive_integer(user_input):
+                self.valid_integers.append(int(user_input))
